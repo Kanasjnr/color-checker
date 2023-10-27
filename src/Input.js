ac359b66591953826a61fb9f191585e020dcdd4b
Kanasjnr <141998286+Kanasjnr@users.noreply.github.com>
2023-10-27 10:46:58 +0100

    update

---
+++ src/Input.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Input = ({colorValue, setColorValue, setHexValue}) => {
+  return (
+  <form onSubmit={(e) => e.preventDefault()}>
+    <label>Add Color Name: </label>
+    <input
+        autoFocus
+        type="text"
+        required
+        placeholder="Add color name"
+        value={colorValue}
+        onChange={(e) => {
+            setColorValue(e.target.value)
+            setHexValue(e.target.value)
+            
+        }}
+
+     >
+       
+
+    </input>
+
+  </form>
+  )
+}
+
+export default Input
---
+++ src/Square.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Square = ({colorValue, hexValue}) => {
+  return (
+  <section className='square' style={{backgroundColor:colorValue}}>
+    <p>
+        {colorValue ? colorValue: "Empty Value"}
+    </p>
+    <p>
+         {hexValue ? hexValue:null}
+
+    </p>
+
+  </section>
+  )
+}
+
+Square.defaultProps = {
+    colorValue:"Empty Colour Value"
+}
+
+export default Square
